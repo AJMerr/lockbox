828219bd1e2203904aab3d2b8621f996470a1902
Austin Merrill <austinm9506@gmail.com>
2025-11-06 02:31:25 -0500

    Structured data shape for vault.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, Subcommand};
+use serde::{Deserialize, Serialize};
+use std::{
+    fs::{self, File},
+    io::{BufReader, Write},
+    path::PathBuf,
+};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Vault {
+    id: usize,
+    service: String,
+    username: String,
+    password: String,
 }
+
+fn main() {}
