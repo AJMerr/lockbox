8c0a9ba40ccb65145204399291c8c69af9e9cbff
Austin Merrill <austinm9506@gmail.com>
2025-11-07 01:18:39 -0500

    Added data shape for JSON store. Implemented load and save methods.

--- src/main.rs
+++ src/main.rs
@@ -25,4 +25,47 @@ impl Vault {
     }
 }
 
+#[derive(Debug, Default, Serialize, Deserialize)]
+struct Store {
+    next_id: usize,
+    vault_items: Vec<Vault>,
+}
+
+impl Store {
+    fn load(path: &PathBuf) -> Self {
+        if !path.exists() {
+            return Store {
+                next_id: 1,
+                vault_items: vec![],
+            };
+        }
+        let file = match File::open(path) {
+            Ok(f) => f,
+            Err(_) => {
+                return Store {
+                    next_id: 1,
+                    vault_items: vec![],
+                };
+            }
+        };
+        let reader = BufReader::new(file);
+        serde_json::from_reader(reader).unwrap_or_else(|_| Store {
+            next_id: 1,
+            vault_items: vec![],
+        })
+    }
+
+    fn save(&self, path: &PathBuf) -> std::io::Result<()> {
+        let json = serde_json::to_vec_pretty(self).expect("serialize_error");
+        let tmp = path.with_extension("json.tmp");
+        {
+            let mut f = File::create(&tmp)?;
+            f.write_all(&json)?;
+            f.flush()?;
+        }
+        fs::rename(tmp, path)?;
+        Ok(())
+    }
+}
+
 fn main() {}
